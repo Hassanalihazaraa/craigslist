876f9d726eaf02b7d7f4e7b349dbd17ec22f0113
Hassan <hassanalihazaraa@gmail.com>
2020-11-16 22:24:15 +0100

    get image of the craigslist items and display it on the results

--- myapp/views.py
+++ myapp/views.py
@@ -5,6 +5,7 @@ from requests.compat import quote_plus
 from . import models
 
 BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
 
 def home(request):
@@ -26,10 +27,20 @@ def search(request):
     for post in post_listing:
         post_title = post.find(class_='result-title').text
         post_url = post.find('a').get('href')
-        post_price = post.find(class_='result-price').text
-        final_posting.append((post_title, post_url, post_price))
 
-    print(final_posting)
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = 'N/A'
+
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_IMAGE_URL.format(post_image_id)
+        else:
+            post_image_url = 'https://craigslist.org/images/peace.jpg'
+
+        final_posting.append((post_title, post_url, post_price, post_image_url))
+
     context = {
         'search': search_content,
         'final_postings': final_posting,
